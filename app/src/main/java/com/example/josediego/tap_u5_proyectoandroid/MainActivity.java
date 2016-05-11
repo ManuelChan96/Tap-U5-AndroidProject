501e4068d2465a07b6bde53348095455bf0801ad
Jose Diego <DIEGOmai1995@hotmail.com>
2016-05-11 15:12:52 -0600

    Proyecto con la primera vista

--- app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.josediego.tap_u5_proyectoandroid;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tabla tabla = new Tabla(this, (TableLayout)findViewById(R.id.tabla));
+        tabla.agregarCabecera(R.array.cabecera_tabla);
+        for(int i = 0; i < 15; i++)
+        {
+            ArrayList<String> elementos = new ArrayList<String>();
+            elementos.add(Integer.toString(i));
+            elementos.add("Casilla [" + i + ", 0]");
+            elementos.add("Casilla [" + i + ", 1]");
+            elementos.add("Casilla [" + i + ", 2]");
+            elementos.add("Casilla [" + i + ", 3]");
+            tabla.agregarFilaTabla(elementos);
+        }
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
