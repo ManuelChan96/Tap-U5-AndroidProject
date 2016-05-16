7dca2f799e2ba18cfa9832c570a941e06c8cc969
IvanHerrera15 <heivherreraba@ittepic.edu.mx>
2016-05-16 13:37:57 -0600

    Navigation Drawer y opción de búsqueda

--- app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.josediego.tap_u5_proyectoandroid;
 
 
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TableLayout;
+
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] items;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +42,73 @@ public class MainActivity extends AppCompatActivity {
             tabla.agregarFilaTabla(elementos);
         }
 
+        items = getResources().getStringArray(R.array.items_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        View header= getLayoutInflater().inflate(R.layout.header, null);
+        mDrawerList.addHeaderView(header);
+
+        drawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, R.string.abierto, R.string.cerrado) {
+            @Override
+            public void onDrawerClosed(View draweView){
+                super.onDrawerClosed(draweView);
+                ActivityCompat.invalidateOptionsMenu(MainActivity.this);
+            }
+
+            @Override
+            public void onDrawerOpened(View draweView){
+                super.onDrawerOpened(draweView);
+                ActivityCompat.invalidateOptionsMenu(MainActivity.this);
+
+            }
+
+        };
+
+        mDrawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, items));
+        // Set the list's click listener
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
-    /*@Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        int id = item.getItemId();
+
+        /*//noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
+        final MenuItem searchItem= menu.findItem(R.id.menu_buscar);
+        final SearchView searchView= (SearchView) MenuItemCompat.getActionView(searchItem);
         return true;
-    }*/
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
 }
