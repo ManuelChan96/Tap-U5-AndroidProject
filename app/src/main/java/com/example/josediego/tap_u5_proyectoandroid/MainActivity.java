4b691e0517332e7a79c0831fb8afca2c5196c3cd
agustinbarajas <agbarajasva@ittepic.edu.mx>
2016-05-18 13:56:03 -0600

    Programa con vista agregar nuevo objeto y evento

--- app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/josediego/tap_u5_proyectoandroid/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.josediego.tap_u5_proyectoandroid;
 
-
-import android.content.ClipData.*;
 import android.content.Intent;
-import android.location.GpsStatus;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,18 +9,14 @@ import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TableLayout;
-
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     private String[] items;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -35,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Tabla tabla = new Tabla(this, (TableLayout)findViewById(R.id.tabla));
         tabla.agregarCabecera(R.array.cabecera_tabla);
-        for(int i = 0; i < 15; i++)
-        {
+        for(int i = 0; i < 15; i++){
             ArrayList<String> elementos = new ArrayList<String>();
             elementos.add(Integer.toString(i));
             elementos.add("Casilla [" + i + ", 0]");
@@ -45,13 +37,11 @@ public class MainActivity extends AppCompatActivity {
             elementos.add("Casilla [" + i + ", 3]");
             tabla.agregarFilaTabla(elementos);
         }
-
         items = getResources().getStringArray(R.array.items_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        //View header= getLayoutInflater().inflate(R.layout.header, null);
-        //mDrawerList.addHeaderView(header);
-
+        View header= getLayoutInflater().inflate(R.layout.header, null);
+        mDrawerList.addHeaderView(header);
         drawerToggle = new ActionBarDrawerToggle(this,
                 mDrawerLayout, R.string.abierto, R.string.cerrado) {
             @Override
@@ -59,54 +49,35 @@ public class MainActivity extends AppCompatActivity {
                 super.onDrawerClosed(draweView);
                 ActivityCompat.invalidateOptionsMenu(MainActivity.this);
             }
-
             @Override
             public void onDrawerOpened(View draweView){
                 super.onDrawerOpened(draweView);
                 ActivityCompat.invalidateOptionsMenu(MainActivity.this);
-
             }
-
         };
-
         mDrawerLayout.setDrawerListener(drawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, items));
         // Set the list's click listener
         //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        /*MenuItem add = (MenuItem)findViewById(R.id.new_Object_id);
-        add.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                agregarNuevo();
-                return true;
-            }
-        });*/
     }
-
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         int id = item.getItemId();
-
         /*//noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }*/
-
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
@@ -122,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
         });
         return true;
     }
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -132,7 +102,4 @@ public class MainActivity extends AppCompatActivity {
         intent = new Intent(this, NewObject.class);
         startActivity(intent);
     }
-    /*public void OnClickNuevo(View vista){
-        this.agregarNuevo();
-    }*/
 }
